0bd30387805b1df5e1ab052e9fee79d638416769
Bized shiekh100 <111190288+Baized100@users.noreply.github.com>
2022-08-30 11:46:29 +0600

    Add files via upload

---
+++ positive &negative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+
+
+int a;
+printf("Enter any number  :");
+scanf("%d",&a);
+if(a>0)
+    printf("a is a positive :");
+else
+    printf(" a is a negative number ");
+getch();
+}
